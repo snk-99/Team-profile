29ffb38391c5faebac387c11da6349a1fe3a6ab1
samuel kahura <kahurasamuel@gmail.com>
2021-06-30 18:29:43 -0400

    final

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-
 const fs = require("fs");
 
+const render = require("./lib/htmlRenderer");
+const { run } = require("jest");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "index.html");
 
-const render = require("./lib/htmlRenderer");
-const { run } = require("jest");
 
 const teamProfile = [];
 
